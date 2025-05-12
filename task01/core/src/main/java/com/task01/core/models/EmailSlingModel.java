7cf9dc807df11572297d433b8d6271e8f9450415
SaiAnjuri26 <anjurisaishyam@gmail.com>
2025-05-12 11:47:28 +0530

    Email-Component-Added

---
+++ task01/core/src/main/java/com/task01/core/models/EmailSlingModel.java
@@ -0,0 +1,22 @@
+package com.task01.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class EmailSlingModel {
+    @ValueMapValue
+    private String email;
+    @ValueMapValue
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
