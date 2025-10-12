5c15edeb9a1663790744d90427b9d2b6cacf0bd1
SaiAnjuri26 <anjurisaishyam@gmail.com>
2025-10-12 10:24:30 +0530

    ResourceResolver

---
+++ core/src/main/java/com/task01/core/servlets/ResourceResolverPratice.java
@@ -0,0 +1,38 @@
+package com.task01.core.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = Servlet.class, immediate = true)
+@SlingServletResourceTypes(resourceTypes = "sai/shyam/resourceResolver/pratice", extensions = "json")
+public class ResourceResolverPratice extends SlingAllMethodsServlet {
+    @Reference
+    ResourceResolverFactory resolverFactory;
+    private static final Logger log = LoggerFactory.getLogger(ResourceResolverPratice.class);
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        HashMap<String, Object> parmMap = new HashMap<>();
+        parmMap.put(resolverFactory.SUBSERVICE, "saishyam");
+        
+
+        
+
+    }
+
+}
